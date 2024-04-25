8cf504f97403a0e850b6b1a75954142666a76db2
yahweh15 <167311552+yahweh15@users.noreply.github.com>
2024-04-25 14:04:28 -0700

    Update cherotich code.ino

--- cherotich code.ino
+++ cherotich code.ino
@@ -4,7 +4,7 @@
 LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
 const int relay_Pin = 2;
 const int buzzer_Pin = 3;
-const int ir_Sensor = 10;
+const int ultrasonic_Sensor = 10;
 const int vibration_Sensor = 11;
 
 TinyGPS gps;
@@ -15,7 +15,7 @@ bool vibration_status = LOW;
 void setup() {
   pinMode(relay_Pin, OUTPUT);
   pinMode(buzzer_Pin, OUTPUT);
-  pinMode(ir_Sensor, INPUT);
+  pinMode(Ultrasonic_Sensor, INPUT);
   pinMode(vibration_Sensor, INPUT);
 
   Serial.begin(9600);
@@ -26,18 +26,13 @@ void setup() {
 }
 
 void loop() {
-  ir_status = digitalRead(ir_Sensor);
+  ir_status = digitalRead(Ultrasonic_Sensor);
   delay(100);
   
   if(ir_status == HIGH) {
     digitalWrite(buzzer_Pin, LOW);   
     delay(200);
-    lcd.clear();
-    lcd.print("Seat Belt");
-    lcd.setCursor(3, 2);
-    lcd.print("Detected");
-    delay(500);  
-    
+  
     digitalWrite(relay_Pin, HIGH);  // Turn on the relay
     
     while(1) {
@@ -49,16 +44,16 @@ void loop() {
         lcd.print("Accident Detected");
         lcd.setCursor(3, 2);
         lcd.print("Sending Msg");
-        delay(500);    
+        delay(100);    
         
         digitalWrite(relay_Pin, LOW);   // Turn off the relay
         digitalWrite(buzzer_Pin, HIGH); // Turn on the buzzer
         
         Serial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
         delay(1000);  // Delay of 1000 milli seconds or 1 second
-        Serial.println("AT+CMGS=\"+254724904084\"\r"); // Replace x with mobile number
+        Serial.println("AT+CMGS=\"+251993015010\"\r"); // Replace x with mobile number
         delay(1000);
-        Serial.println("Accident Detected, please check location");// The SMS text you want to send
+        Serial.println("Accident Detected, please check your nearby location");// The SMS text you want to send
         while(1) {
           gps_read();
         }
@@ -67,17 +62,7 @@ void loop() {
         digitalWrite(buzzer_Pin, LOW);  // Turn off the buzzer
       }
     }
-  } else {
-    lcd.clear();
-    lcd.print("Seat Belt");
-    lcd.setCursor(3, 2);
-    lcd.print("not Detected");
-    digitalWrite(relay_Pin, LOW);   
-    delay(200);              
-    digitalWrite(buzzer_Pin, HIGH);   
-    delay(200);          
   }
-}
 
 void gps_read() { 
   byte a;
